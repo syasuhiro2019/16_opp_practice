34318860f60e1a5b52166a0e543b68d40f79a854
Sato <s_ychm@yahoo.co.jp>
2019-03-11 10:31:07 +0900

    長方形の面積を求める

--- rectangle.py
+++ rectangle.py
@@ -1,9 +1,20 @@
 class Rectangle:
-    def __init__(self):
-        print('Called! __init__')
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
 
 
 if __name__ == '__main__':
-    r1 = Rectangle()
+    # 横:4 縦:3の長方形
+    r1 = Rectangle(4, 3)  # インスタンス化
 
-    r2 = Rectangle()
+    #
+    area1 = r1.area()  # 面積を計算する
+    print(area1)  # 12 が出力される
+
+    r2 = Rectangle(10, 5)  #
+    area2 = r2.area()
+    print(area2)
